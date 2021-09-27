47b59a25f46f6a55ed0cca9ebe600db114291050
Бажен Левковець <bazhen.paseka@gmail.com>
2021-09-27 16:39:03 +0300

    add Local Config

---
+++ Drivers/Local_config/ringbuffer_dma_parol.h
@@ -0,0 +1,20 @@
+/*
+ * ringbuffer_dma_parol.h
+ *
+ *  Created on: 19 ????. 2019 ?.
+ *      Author: Paseka
+ */
+
+#ifndef LOCAL_CONFIG_RINGBUFFER_DMA_PAROL_H_
+#define LOCAL_CONFIG_RINGBUFFER_DMA_PAROL_H_
+
+	#define THINGSPEAK_ADDRESS	"api.thingspeak.com"
+	#define THINGSPEAK_API_KEY	"5ZRXVI7HIJQPGKID"				// Enter your Write API here
+	#define WIFI_SSID			"Tapac"
+	#define WIFI_PASS			"27051329"
+
+	#define	DEVICE	  4
+	#define FORSORT   5
+	#define ITERATION 12
+
+#endif /* LOCAL_CONFIG_RINGBUFFER_DMA_PAROL_H_ */
